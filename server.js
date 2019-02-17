7c77a04081a6b026cec203b2b31179497a4fbf7b
unknown <nbatre1@yahoo.com>
2019-02-17 00:29:21 -0600

    i failed but i am still learning: :

--- server.js
+++ server.js
@@ -17,110 +17,79 @@ var pusher = new Pusher({
 
 
 
+
 var app = express();
 
-
-// cookie parser
+// must use cookieParser before expressSession
 app.use(cookieParser());
 
-
-app.use(expressSession({
-    secret:'<some-secret-token-here>',
-    resave:true,
-    saveUninitialized:true
-
-}));
-
-// turns the  data or information and attach it to bodyParser and turns into a json through bodyparser
+app.use(expressSession({secret:'<some-secret-token-here>',resave: true, saveUninitialized: true}));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// learns this line
-app.use(express.static(path.join(__dirname,'public')));
-
-
-// error handle for 404 pages
-app.use(function(req,res,next){
-    var error = new Error('Route Not Found');
-    error.status = 404;
-    next(error);
-});
-
-
-
-// creating a post to get the information from the req.body user and store it on the register route
-app.post('/register',function(req,res){
-    console.log(req.body);
-    if(req.body.username && req.body.username)
-    {
-        var newMember ={
-            username:req.body.username,
-            status: req.body.status
-        }
-     req.session.user = newMember;
-     res.json({
-         success:true,
-         error:false
-     });
+app.post('/register', function(req, res){
+  console.log(req.body);
+  if(req.body.username && req.body.status){
+    var newMember = {
+      username: req.body.username,
+      status: req.body.status
     }
-     else{
-         res.json({
-             success:false,
-             error:true,
-             message:'Incomplete information: username and status required'
-         });
-     }
+    req.session.user = newMember;
+    res.json({  
+      success: true,
+      error: false
+    });
+  }else{
+    res.json({  
+      success: false,
+      error: true,
+      message: 'Incomplete information: username and status are required'
+    });
+  }
 });
 
-// authorizing the api on the server side/ enables and client subscribing to Pusher private and presence channels
-
-
-app.post('/usersystem/auth',function(req,res){
-    var socketId = req.body.socket_id;
-    var channel = req.body.channel_name;
-    var currentMember = req.session.user;
-    var presenceData ={
-        user_id:currentMember.username,
-        user_info:{
-            status:currentMember.status,
-        }
-    };
-    var auth = pusher.authenticate(socketId,channel,presenceData);
-    res.send(auth);
+app.post('/usersystem/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var currentMember = req.session.user;
+  var presenceData = {
+    user_id: currentMember.username,
+    user_info: {
+      status: currentMember.status,
+    }
+  };
+  var auth = pusher.authenticate(socketId, channel, presenceData);
+  res.send(auth);
 });
-
-// creating and api route to see if the user is logged in.
 
 app.get('/isLoggedIn', function(req,res){
-    if(req.session.user){
-        res.send({
-            authenticate:true
-        });}
-    else{
-       res.send({authenticated:false});
-    }
+  if(req.session.user){
+    res.send({ 
+      authenticated: true 
+    });
+  }else{
+    res.send({ authenticated: false });
+  }
 });
 
-
-// to logout of the session api
-
-app.get('/logout',function(req,res){
-    if(req.session.user){
-        req.session.user = null;
-    }
-    res.redirect('/');
+app.get('/logout', function(req,res){
+  if(req.session.user){
+    req.session.user = null;
+  }
+  res.redirect('/');
 });
 
-
-
-
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+    var error404 = new Error('Route Not Found');
+    error404.status = 404;
+    next(error404);
+});
 
 module.exports = app;
 
-var PORT = process.env.PORT || 5000
-
-
-app.listen(PORT,function(){
-    console.log(`listen on ${PORT}`)
-})
+app.listen(9000, function(){
+  console.log('Example app listening on port 9000!')
+});
