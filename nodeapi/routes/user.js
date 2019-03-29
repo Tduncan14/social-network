a2cfb32f6ca8e5aef122e75bc46a587f10c7c105
unknown <nbatre1@yahoo.com>
2019-03-29 16:30:30 -0500

    keep moving forward

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const{userById,allUsers,getUser,updateUser} = require("../controllers/user");
+const{userById,allUsers,getUser,updateUser,deleteUser} = require("../controllers/user");
 const {requireSignin} = require("../controllers/auth");
 
 
@@ -8,8 +8,8 @@ const router = express.Router();
 
 router.get("/users",allUsers);
 router.get("/user/:userId",requireSignin,getUser);
-router.put("/user/:userId",requireSignin,updateUser)
-
+router.put("/user/:userId",requireSignin,updateUser);
+router.delete("/user/:userId",requireSignin,deleteUser);
 
 // any route containing:userId, our app will first execute userByid()
 router.param("userId",userById);
