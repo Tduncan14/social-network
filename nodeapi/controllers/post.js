d2458172066a4f8d99857d1ba4f58cb7dbcf356a
unknown <nbatre1@yahoo.com>
2019-04-08 15:56:10 -0500

    keep moving forward

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,9 +1,36 @@
 const Post = require('../models/post');
 const formidable =require('formidable');
+const _ = require('lodash');
+
 const fs = require('fs')
+
+
+
+exports.postById = (req,res,next,id) =>{
+    Post.findById(id)
+    .populate("postedBy","_id name")
+    .exec((err,post) =>{
+        if(err || !post){
+            res.status(400).json({
+                err:err
+            })
+        }
+        req.post = post
+        next();
+    })
+}
+
+
+
+
+
+
 exports.getPosts =(req,res,)=>{
   
-    const posts = Post.find().select("_id title body")
+    const posts = Post.find()
+    // gets the users name for the post
+     .populate("postedBy","_id name")
+    .select(" _id title body")
     .then((posts) =>{
         res.json({posts:posts})
     })
@@ -21,7 +48,8 @@ exports.createPost =(req,res,next) =>{
          res.status(400).json({error:"Image could not be uploaded"})
      }
       post = new Post(fields)
-      
+
+      console.log("req.profile:", req.profile)
       req.profile.hashed_password = undefined;
       req.profile.salt = undefined;
 
@@ -54,4 +82,71 @@ exports.createPost =(req,res,next) =>{
 //          post:result
 //      })
 //  })
+ };
+
+ exports.postByUser = (req,res) =>{
+     Post.find({postedBy:req.profile._id})
+         .populate("postedBy","_id name")
+         .sort("_created")
+         .exec((err,posts) =>{
+             if(err){
+                 return res.status(400).json({
+                     error:err
+                 })
+              
+             }
+             res.json(posts);
+         })
  }
+
+
+ // if poster the correct poster allow to delete or not
+ exports.isPoster =(req,res,next) =>{
+     let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+
+     if(!isPoster){
+         return res.status(403).json({err:"user is not authorized"})
+     }
+
+     next();
+ }
+
+// update the user post
+
+exports.updatePost = (req,res,next) =>{
+    let post = req.post
+    post = _.extend(post,req.body)
+    post.updated = Date.now()
+    post.save((err) =>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            })
+        }
+        res.json(post)
+    })
+   
+}
+
+
+
+// delete the post
+exports.deletePost = (req,res) =>{
+    let post = req.post;
+
+    console.log("req post", req.post);
+    console.log("req auth", req.auth);
+    console.log("req.post.postedBy._id", req.post.postedBy._id);
+    console.log("req.auth._id", req.auth._id);
+
+    post.remove((err,post)=>{ 
+       if(err || !post){
+           res.status(400).json({
+               err:err,
+           })
+       }
+       res.json({msg:"post deleted"})
+    });
+}
+
+
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 
-const {getPosts, createPost }= require('../controllers/post');
+const {postById,getPosts, createPost,postByUser, isPoster,deletePost,updatePost }= require('../controllers/post');
 const {createPostValidator} = require('../validator');
 const{userById} = require("../controllers/user");
 const{requireSignin} = require('../controllers/auth');
@@ -8,9 +8,15 @@ const router = express.Router();
 
 
 router.get("/",getPosts);
+router.get("/posts/by/:userId",postByUser);
 router.post("/post/new/:userId",requireSignin,createPost,createPostValidator);
 
+router.put('/post/:postId',requireSignin,updatePost);
 
+router.delete('/post/:postId',requireSignin,isPoster,deletePost)
+// any route containing :userId, our app will first execute userById()
 router.param("userId",userById);
+// any route containing :postId, our app will first execute postsById()
+router.param("postId",postById)
 
 module.exports = router;
