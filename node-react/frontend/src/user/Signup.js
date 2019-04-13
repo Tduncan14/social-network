48f3520be9a9554ea4b096e07471a2a2bef0954f
unknown <nbatre1@yahoo.com>
2019-04-13 13:28:49 -0500

    keep moving forward

--- node-react/frontend/src/user/Signup.js
+++ node-react/frontend/src/user/Signup.js
@@ -14,16 +14,48 @@ class Signup extends Component {
          }
     }
     handleChange=(name)=> event => {
-   
-
       this.setState({
            [name]:event.target.value
       })
-
- 
      }
 
+    clickSubmit = (event) =>{
+        event.preventDefault()
+        const {name,email,password} = this.state;
+
+        const user = {
+            name,
+            email,
+            password
+        }
+       // console.log(user)
+
+      // Using fetch to make a post call to backend.This sending data to create a new user
+    // then you define a method with an object// easy to configure
+      fetch("http://localhost:8080/signup",{
+          method:"POST",
+          headers:{
+             Accept:"application/json",
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify(user)
+      })
+        .then(response =>{
+            return response.json()
+        })
+        .catch(err => console.log(err));
+
+        this.setState({
+          name:"",
+          email:"",
+          password:""
+        })
+     
+    }
+
+
     render(){
+        const {name,email,password} = this.state
         return(
             <div className="container">
            <h2 className="mt-5 mb-5">
@@ -33,19 +65,19 @@ class Signup extends Component {
            <div className="form-group">
             <label className="text-muted">Name:</label>
             <input onChange={this.handleChange("name")} className="form-control" type="text"
-             value={this.state.name}/>
+             value={name}/>
            </div>
            <div className="form-group">
             <label className="text-muted">Email:</label>
             <input onChange={this.handleChange("email")} className ="form-control" type="email"
-             value={this.state.email}/>
+             value={email}/>
            </div>
            <div className="form-group">
            <label className="text-muted">Password:</label>
            <input onChange={this.handleChange("password")} className ="form-control" type="password" 
-            value={this.state.password}/>
+            value={password}/>
            </div>
-           <button className="btn btn-raised btn-primary">Submit</button>
+           <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">Submit</button>
            </form>
             </div>
         )
